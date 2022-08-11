87111c6fa5b5d2286fadfdf98692ed57ed26f905
Skylan Lew <skylanslew@gmail.com>
2022-08-11 14:42:28 -0700

    added another calculator for a lesson
    Co-authored-by: Brandon Spear <bowdowntotheepicturkey@gmail.com>

--- js/scripts.js
+++ js/scripts.js
@@ -3,8 +3,30 @@ function add(number1, number2) {
   return number1 + number2;
 }
 
+function subtract(number1, number2) {
+  return number1 - number2;
+}
+
+function divide(number1, number2) {
+  return number1 / number2;
+}
+
+function multiply(number1, number2) {
+  return number1 * number2;
+}
+
+function remainder(number1, number2) {
+  return number1 % number2;
+}
+
 // interface logic
 const number1 = parseFloat(prompt("Enter a number:"));
 const number2 = parseFloat(prompt("Ener another number:"));
 
-window.alert(add(number1, number2));
+window.alert(
+  number1 + " + " + number2 + " = " + add(number1, number2) + "\n" + 
+  number1 + " - " + number2 + " = " + subtract(number1, number2) + "\n" +
+  number1 + " * " + number2 + " = " + multiply(number1, number2) + "\n" +
+  number1 + " / " + number2 + " = " + divide(number1, number2) + "\n" +
+  number1 + " % " + number2 + " = " + remainder(number1, number2)
+  );
